c8916ae32dfa63525c2923e38f56e34e38df0cc6
maksad <m90x@mail.ru>
2017-03-06 22:12:43 +0200

    Add "angular2-webpack"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { provideRoutes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ApiService } from './shared';
 import { AppComponent } from './app.component';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-describe('AppComponent', function () {
-  let de: DebugElement;
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-  }));
-
+describe('App', () => {
+  // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [ApiService, provideRoutes([])]
+    });
   });
 
-  it('should create component', () => expect(comp).toBeDefined() );
-
-  it('should have expected <h1> text', () => {
+  it('should have an url', () => {
+    let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    expect(fixture.debugElement.componentInstance.url).toEqual('https://github.com/preboot/angular2-webpack');
   });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  url = 'https://github.com/preboot/angular2-webpack';
+  title: string;
+
+  constructor(private api: ApiService) {
+    this.title = this.api.title;
+  }
+}
