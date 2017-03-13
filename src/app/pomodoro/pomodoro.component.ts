8e248f065828a3a646db357494643eb9ed7b1f2b
maksad <m90x@mail.ru>
2017-03-14 00:19:28 +0200

    Add Increase/Reduce Timer functions

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -5,4 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './pomodoro.component.html',
   styleUrls: ['./pomodoro.component.scss']
 })
-export class PomodoroComponent {}
+export class PomodoroComponent {
+  public breakTime = 5;
+  public sessionTime = 25;
+  public countDownTimer = 0;
+
+  reduceBreakTime() {
+    if (this.breakTime > 1) {
+      this.breakTime -= 1;
+    }
+  }
+
+  increaseBreakTime() {
+    this.breakTime += 1;
+  }
+
+  reduceSessionTime() {
+    if (this.sessionTime > 1) {
+      this.sessionTime -= 1;
+    }
+  }
+
+  increaseSessionTime() {
+    this.sessionTime += 1;
+  }
+
+  startCountDown(sessionT: number, breakT: number) {
+    console.log(sessionT, breakT);
+  }
+}
