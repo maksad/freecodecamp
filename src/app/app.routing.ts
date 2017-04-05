eb6112a46d1a5fced6f03104feb3187d8662745b
maksad <m90x@mail.ru>
2017-04-05 23:26:50 +0300

    Initialise SimonComponent

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { HomeComponent } from './home/home.component';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { SimonComponent } from './simon/simon.compnent';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'pomodoro', component: PomodoroComponent },
   { path: 'tic-tac-toe', component: TicTacToeComponent },
+  { path: 'simon', component: SimonComponent },
 ];
 
 export const routing = RouterModule.forRoot(routes);
